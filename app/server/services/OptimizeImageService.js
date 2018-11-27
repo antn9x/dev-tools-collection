1b6dc5290748af7379993db88ec2191db82f79d2
An.Ta <antn9x@gmail.com>
2018-11-27 13:10:54 +0700

    fix optimize image service

--- app/server/services/OptimizeImageService.js
+++ app/server/services/OptimizeImageService.js
@@ -1,15 +1,40 @@
-const imagemin = require('imagemin');
-const imageminJpegtran = require('imagemin-jpegtran');
-const imageminPngquant = require('imagemin-pngquant');
+import imagemin from 'imagemin';
+import imageminJpegtran from 'imagemin-jpegtran';
+import imageminPngquant from 'imagemin-pngquant';
+import path from 'path';
 
-export default (source, destination) => new Promise((resolve, reject) => {
-  const src = `${source}/*.{jpg,png}`;
-  imagemin([src], destination, {
+import Logger from '../utils/Logger';
+
+/**
+ * Create one image resized
+ * @param {string} imgPath 
+ * @param {string} outputDir 
+ * @param {string} name 
+ * @param {number} width 
+ * @param {number} height 
+ */
+export const optimizeImage = (imageData) => {
+  const {
+    source,
+    destination,
+    quality = '65-80',
+    name
+  } = imageData;
+  const fileName = path.resolve(source, name);
+  const newPath = destination ? path.join(destination, name) : fileName;
+  Logger.log(`Filename: ${fileName}`);
+  return imagemin([source], newPath, {
     plugins: [
       imageminJpegtran(),
       imageminPngquant({
-        quality: '65-80',
+        quality,
       }),
     ],
-  }).then(resolve).catch(reject);
-});
+  });
+};
+
+/**
+ * resize a list of images
+ * @param {Array} imageList 
+ */
+export const optimizeListImages = (imageList) => Promise.all(imageList.map(optimizeImage));
