5984a32a2c0821de2bd78ab69d0ae128d4017fa2
ntq <ntq@ntqs-iMac.local>
2018-11-27 13:02:35 +0700

    fix reisze service

--- app/server/services/ResizeImagesService.js
+++ app/server/services/ResizeImagesService.js
@@ -1,20 +1,43 @@
+import path from 'path';
+import sharp from 'sharp';
 import Logger from "../utils/Logger";
 
-const sharp = require('sharp');
+/**
+ * Create one image resized
+ * @param {string} imgPath 
+ * @param {string} outputDir 
+ * @param {string} name 
+ * @param {number} width 
+ * @param {number} height 
+ */
+export const createThumnail = (imageData) => {
+  const {
+    imgPath,
+    outputDir,
+    name,
+    width,
+    height
+  } = imageData;
+  const fileName = path.resolve(imgPath, name);
+  const newPath = outputDir ? path.join(outputDir, name) : fileName;
+  Logger.log(`Filename: ${  fileName}`);
+  return new Promise((resolve, reject) => {
+    sharp(fileName)
+      .resize(width, height)
+      .max()
+      .toFile(newPath, (err) => {
+        if (err) {
+          Logger.error(err);
+          reject(err);
+        }
+        resolve();
+      });
 
-// Command line interface
+  });
+};
 
-export default function createThumnail(imgPath, outputDir){
-    const fileName = imgPath.replace(/^.*[\\/]/, '');
-    const newPath = outputDir + fileName;
-
-    console.log(`Filename: ${  fileName}`);
-
-    sharp(imgPath)
-    .resize(268, 154)
-    .max()
-    .toFile(newPath, (err) => {
-        // imgPath
-        Logger.error(err);
-    });
-}
+/**
+ * resize a list of images
+ * @param {Array} imageList 
+ */
+export const resizeListImages = (imageList) => Promise.all(imageList.map(createThumnail));
