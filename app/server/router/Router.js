36164c19effa0600393dd6715f84457c6083101c
An.Ta <antn9x@gmail.com>
2018-12-01 23:24:23 +0700

    fix router

--- app/server/router/Router.js
+++ app/server/router/Router.js
@@ -1,45 +1,33 @@
-import { ipcMain } from "electron";
-import { GET_FOLDER_FILES, RENAME, MODIFY_EXT, OPTIMIZE, RE_SIZE, CHECK_FILE_EXIST } from "../../constant.message";
-// import onRename from "../services/RenameService";
+import { ipcMain } from 'electron';
+import {
+  GET_FOLDER_FILES,
+  MODIFY_EXT,
+  OPTIMIZE,
+  RE_SIZE,
+  CHECK_FILE_EXIST
+} from '../../constant.message';
+// import onRename from '../services/RenameService';
 import { modifyExt } from '../services/RenameService';
-import {getFilesInFolder, checkFileExist} from "../services/FilesService";
-import OptimizeAllImages from "../services/OptimizeImageService";
-import { resizeAllImages } from "../services/ResizeImagesService";
-// import Logger from "../utils/Logger";
+import {
+  getFilesInFolder,
+  checkFileExist
+} from '../services/FilesService';
+import OptimizeAllImages from '../services/OptimizeImageService';
+import { resizeAllImages } from '../services/ResizeImagesService';
+// import Logger from '../utils/Logger';
 
-const listListeners = [RENAME, GET_FOLDER_FILES, MODIFY_EXT, OPTIMIZE, RE_SIZE, CHECK_FILE_EXIST];
-
-async function handleLister(event, data, name) {
-    let response = '';
-    switch (name) {
-        // case RENAME:
-        //     response = await onRename(data);
-        //     break;
-        case CHECK_FILE_EXIST:
-            response = await checkFileExist(data);
-            break;
-        case GET_FOLDER_FILES:
-            response = await getFilesInFolder(data);
-            break;
-        case MODIFY_EXT:
-            response = await modifyExt(data);
-            break;
-        case RE_SIZE:
-            response = await resizeAllImages(data);
-            break;
-        case OPTIMIZE:
-            response = await OptimizeAllImages(data);
-            break;
-
-        default:
-            break;
-    }
-    // Logger.log('response', response);
-    event.sender.send(name, response);
-}
+const addListener = (name, listener) => {
+    ipcMain.on(name, async (event, data) => {
+        const response = await listener(data);
+        event.sender.send(name, response);
+    });
+};
 
 export default function Router() {
-    listListeners.forEach(listenerName => {
-        ipcMain.on(listenerName, (event, data) => handleLister(event, data, listenerName));
-    });
+  addListener(CHECK_FILE_EXIST, checkFileExist);
+  addListener(GET_FOLDER_FILES, getFilesInFolder);
+  addListener(OPTIMIZE, OptimizeAllImages);
+  addListener(RE_SIZE, resizeAllImages);
+  addListener(MODIFY_EXT, modifyExt);
+//   addListener(RENAME, onRename);
 }
