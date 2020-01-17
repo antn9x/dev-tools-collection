7a7c5cc942746ac21b0a843848ffed73b9c6ae29
antn <antn9x@gmail.com>
2020-01-17 20:51:19 +0700

    fix optimize recusive

--- app/server/services/OptimizeImageService.js
+++ app/server/services/OptimizeImageService.js
@@ -1,25 +1,36 @@
 import imagemin from 'imagemin';
 import imageminJpegtran from 'imagemin-jpegtran';
 import imageminPngquant from 'imagemin-pngquant';
+import dir from 'node-dir';
+
+const subDirPromise = src => new Promise((resolve, reject) => dir.subdirs(src, (err, subDirs) => {
+  if (err) reject(err);
+  resolve(subDirs);
+}));
 
 /**
  * Optimize a list of images
- * @param {Object} imageListData 
+ * @param {Object} imageListData
  */
-const optimizeAllImages = (imageListData) => {
+const optimizeAllImages = async (imageListData) => {
   const {
     src,
     des,
     qualityRange = '65-80',
   } = imageListData;
-  return imagemin([`${src}/*.{jpg,png}`], des, {
-    plugins: [
-      imageminJpegtran(),
-      imageminPngquant({
-        qualityRange,
-      }),
-    ],
-  });
+  const desFolder = des || src;
+  // console.log(subdirs);
+  const subDirs = await subDirPromise(src);
+  return Promise.all(
+    subDirs.map(dirName => imagemin([`${dirName}/*.{jpg,png}`], dirName.replace(src, desFolder), {
+      plugins: [
+        imageminJpegtran(),
+        imageminPngquant({
+          qualityRange,
+        }),
+      ],
+    }))
+  );
 };
 
 export default optimizeAllImages;
